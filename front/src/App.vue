7b83dac4e3e53eb67d31262d594623f695a836fa
Gabriel Silveira <gabrielnovatoo@gmail.com>
2025-11-04 07:50:09 -0300

    creating an example graphics and cleaning stater cli

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,47 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <h1>Gráficos de Impacto de IA no Mercado de Trabalho</h1>
+    <BarChart />
+  </div>
 </template>
 
+<script setup>
+import BarChart from './components/BarChart.vue'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  text-align: center;
+  margin-top: 60px;
 }
 </style>
