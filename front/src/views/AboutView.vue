134a120fe41dfaf2eb75d459547a007661b21a98
Gabriel Silveira <gabrielnovatoo@gmail.com>
2025-11-05 09:41:46 -0300

    put text in the link page

--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -5,8 +5,8 @@
     <p>Como projeto, resolvemos fazer um site com front-end em Vue.js e back-end em Python com PySpark, para fazermos uma análise dos dados do tema escolhido pela equipe, que é sobre o Impacto de IA no mercado de trabalho.</p>
     
     <h2>Data Sets encontrados e usados no projeto:</h2>
-    <p>https://www.kaggle.com/datasets/sahilislam007/ai-impact-on-job-market-20242030</p>
-    <p>https://www.weforum.org/publications/the-future-of-jobs-report-2025/</p>
+    <p><a href="https://www.kaggle.com/datasets/sahilislam007/ai-impact-on-job-market-20242030" target="_blank">Ai Impact on Job Market</a></p>
+    <p><a href="https://www.weforum.org/publications/the-future-of-jobs-report-2025/" target="_blank">The future of Jobs report 2025</a></p>
     
     <div class="alunos">
       <h3>Alunos:</h3>
